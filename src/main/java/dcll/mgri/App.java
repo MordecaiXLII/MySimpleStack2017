3945d1b96bed91dafa411b40ff63e053daf13994
MordecaiXLII <mordecaixlii@gmail.com>
2016-11-03 10:27:23 +0100

    first commit

---
+++ src/main/java/dcll/mgri/App.java
@@ -0,0 +1,11 @@
+package dcll.mgri;
+
+/**
+ * Created by Mayeul Grivet-Roux on 10/21/2016.
+ */
+public class App {
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
